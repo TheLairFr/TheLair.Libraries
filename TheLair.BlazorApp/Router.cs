59f5873a1323ea90308ebeb9cab5dc532ac48d0c
Nicolas VAREILLE <nicolas.vareille@epitech.eu>
2023-09-18 17:14:32 +0200

    Expose URI BlazorApp

--- TheLair.BlazorApp/Router.cs
+++ TheLair.BlazorApp/Router.cs
@@ -14,6 +14,8 @@ public class Router
 {
     private readonly NavigationManager NavigationManager;
 
+    public string Uri => NavigationManager.Uri;
+
     public Router(NavigationManager navigationManager)
     {
         NavigationManager = navigationManager;
